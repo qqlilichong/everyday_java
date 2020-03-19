341c8e9efa7144b4a2125e1ba4bc8a7069b8a1cb
lilc-a <lilc-a@glodon.com>
2020-03-19 13:59:43 +0800

    update.

--- Main.java
+++ Main.java
@@ -13,6 +13,21 @@ interface Teacher
     void listen();
 }
 
+enum Planet {
+    EARTH(1101),
+    MARS(1102);
+
+    private final int number;
+    Planet(int num)
+    {
+        this.number = num;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+}
+
 public class Main {
 
     static void test_j5() {
@@ -52,6 +67,14 @@ public class Main {
         for (var i : ilist)
         {
             System.out.println(i);
+        }
+
+        // 类型安全枚举
+        System.out.println(Planet.valueOf("EARTH").getNumber());
+        System.out.println(Planet.valueOf("MARS").getNumber());
+        for (var p : Planet.values())
+        {
+            System.out.printf("Planet %s : %d%n", p, p.getNumber());
         }
 
         System.out.println("$$$$$$$$\tprac-test-java5-end\t$$$$$$$$");
